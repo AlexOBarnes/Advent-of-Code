b912ce0f5865d19fbfdb310d04b79b9a3079eb89
Alex Barnes <alex.barnes@vitruvianpartners.com>
2025-12-02 13:53:38 +0000

    AB | Reattempt with regex

--- 2025/solutions/day2.py
+++ 2025/solutions/day2.py
@@ -1,7 +1,8 @@
 """Solution for day 2 problem advent of code https://adventofcode.com/2025/day/2"""
 
-import sys
 import os
+import re
+import sys
 
 sys.path.append(
     os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "src"))
@@ -9,34 +10,24 @@ sys.path.append(
 from aoc import Client
 
 
-def is_double_sequence(num):
-    """Checks if a sequence is a duplicated string of numbers"""
+def is_repeated(num, check_for_multiple_repeats=False):
+    """Checks if a number is a repeated sequence using regex.
+    If check_for_multiple_repeats is True, checks for multiple repeats.
+    """
     s = str(num)
-    n = len(s)
-    if n % 2 != 0:
-        return False
-    half = n // 2
-    return s[:half] == s[half:]
+    pattern = r"(\d+)\1+" if check_for_multiple_repeats else r"(\d+)\1"
+    return bool(re.fullmatch(pattern, s))
 
 
-def is_repeated_sequence(num):
-    """Checks if a number is made up of a repeated string of numbers"""
-    s = str(num)
-    n = len(s)
-    for size in range(1, n // 2 + 1):
-        if n % size == 0:
-            if s == s[:size] * (n // size):
-                return True
-    return False
-
-
-def id_iterator(ids: list, checker=is_double_sequence):
-    """Iterates through a an id range"""
-    total = 0
-    for i in range(int(ids[0]), int(ids[1]) + 1):
-        if checker(i):
-            total += i
-    return total
+def id_iterator(ids: list, check_multiples=False):
+    """Iterates through an id range"""
+    return sum(
+        [
+            i
+            for i in range(int(ids[0]), int(ids[1]) + 1)
+            if is_repeated(i, check_multiples)
+        ]
+    )
 
 
 def part_one_main():
@@ -46,7 +37,7 @@ def part_one_main():
         test=False,
         fn=lambda line: [id_range.split("-") for id_range in line.split(",")],
     )
-    return sum([id_iterator(id_range) for id_range in id_ranges])
+    return sum([id_iterator(id_range, False) for id_range in id_ranges])
 
 
 def part_two_main():
@@ -56,9 +47,7 @@ def part_two_main():
         test=False,
         fn=lambda line: [id_range.split("-") for id_range in line.split(",")],
     )
-    return sum(
-        [id_iterator(id_range, checker=is_repeated_sequence) for id_range in id_ranges]
-    )
+    return sum([id_iterator(id_range, True) for id_range in id_ranges])
 
 
 if __name__ == "__main__":
